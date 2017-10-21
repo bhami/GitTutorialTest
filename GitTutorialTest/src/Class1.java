8853f7824ff6b15cbaaf8aadc26f6769feaee5d3
Bhami <jainbhami@gmail.com>
2017-10-21 17:32:27 -0400

    second change after first commit

--- GitTutorialTest/src/Class1.java
+++ GitTutorialTest/src/Class1.java
@@ -3,5 +3,6 @@ public class Class1 {
 	public static void main(String[] args){
 		System.out.println("Testing git tutorial");
 		System.out.println("First change after initial commit");
+		System.out.println("Second change after first commit");
 	}
 }
